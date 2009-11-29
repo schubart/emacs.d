0281b030781d174061c82e79a9f2073a22c4ab3e
Michael Schubart <michael@schubart.net>
2009-11-29 20:40:55 +0000

    Tabs, smooth scrolling, C++, Perl.

--- init.el
+++ init.el
@@ -16,10 +16,44 @@
 ;; Show column number in status bar.
 (setq column-number-mode t)
 
+;; Don't use tabs.
+;; http://www.jwz.org/doc/tabs-vs-spaces.html
+(setq-default indent-tabs-mode nil)
+
 ;; ido mode for visiting files and switching buffers.
 (ido-mode t)
 ;; Any item containing the entered characters matches.
 (setq ido-enable-flex-matching t)
+
+;; Replace default buffer menu (C-x C-b) with ibuffer.
+(defalias 'list-buffers 'ibuffer)
+
+;; Smooth scrolling.
+;; http://www.emacswiki.org/emacs/SmoothScrolling
+(setq scroll-step 1
+      scroll-conservatively 10000)
+
+;;
+;; C++
+;;
+
+;; Default:     Here:
+;; -----------  ------------
+;; if (a == b)  if (a == b)
+;;   {          {
+;;     foo();     foo();
+;;   }          }
+(c-set-offset 'substatement-open 0)
+
+;;
+;; Perl
+;;
+
+;; cperl-mode is supposedly superior to perl-mode:
+;; http://www.emacswiki.org/emacs/CPerlMode
+(defalias 'perl-mode 'cperl-mode)
+;; Indent with 4 chars as per perlstyle(1).
+(setq cperl-indent-level 4)
 
 ;; Settings specific to my home PC.
 (if (file-exists-p "~/.emacs.d/home.el") (load-library "~/.emacs.d/home.el"))
